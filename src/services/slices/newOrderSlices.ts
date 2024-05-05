c33d0b2dbfdc7463411388c22633ed51e9b4a3a8
Nikita Akmaikin <NikitaAkmaikin-F@yandex.ru>
2024-05-05 07:26:26 +0500

    Напиcал тесты для newOrderSlices и orderSlices

--- src/services/slices/newOrderSlices.ts
+++ src/services/slices/newOrderSlices.ts
@@ -5,13 +5,13 @@ import { orderBurgerApi } from '../../utils/burger-api';
 export const getApiBurgerOrder = createAsyncThunk('order/new', orderBurgerApi);
 
 type TNewOrderState = {
-  orderModalData: TOrder | null;
+  order: TOrder | null;
   name: string;
   orderRequest: boolean;
 };
 
 export const initialState: TNewOrderState = {
-  orderModalData: null,
+  order: null,
   name: '',
   orderRequest: false
 };
@@ -23,7 +23,7 @@ const newOrderSlice = createSlice({
     clearOrder: (state) => (state = initialState)
   },
   selectors: {
-    getOrderModalDataNewOrder: (state) => state.orderModalData,
+    getOrderModalDataNewOrder: (state) => state.order,
     getNameNewOrder: (state) => state.name,
     getOrderRequestNewOrder: (state) => state.orderRequest
   },
@@ -37,7 +37,7 @@ const newOrderSlice = createSlice({
       })
       .addCase(getApiBurgerOrder.fulfilled, (state, action) => {
         state.orderRequest = false;
-        state.orderModalData = action.payload.order;
+        state.order = action.payload.order;
         state.name = action.payload.name;
       });
   }
